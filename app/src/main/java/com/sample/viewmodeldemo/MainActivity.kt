2de357e364bb2b4bdbaf6424056dd1fa3b42ddd4
harshal <NA>
2020-08-17 20:07:55 +0700

    Commit 1

--- app/src/main/java/com/sample/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/sample/viewmodeldemo/MainActivity.kt
@@ -3,21 +3,21 @@ package com.sample.viewmodeldemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.sample.viewmodeldemo.databinding.ActivityMainBinding
-//ViewModel Sample
+
+//Complete ViewModel,LiveData with Data Binding Example
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var viewModel : MainActivityViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-
-        binding.countText.text = viewModel.getCurrentCount().toString()
-        binding.button.setOnClickListener {
-            binding.countText.text = viewModel.getUpdatedCount().toString()
-        }
+        binding.lifecycleOwner = this
+        binding.myViewModel = viewModel
+        
     }
 }
