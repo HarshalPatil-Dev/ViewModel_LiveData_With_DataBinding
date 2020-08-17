9492df962b5203095deac7dfd5cb11ca74a1f9e1
Harshal <harshal@Harshals-MacBook-Pro.local>
2020-08-17 13:35:24 +0700

    Initial commit

---
+++ app/src/main/java/com/sample/viewmodeldemo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sample.viewmodeldemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.sample.viewmodeldemo.databinding.ActivityMainBinding
+//ViewModel Sample
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel : MainActivityViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.countText.text = viewModel.getCurrentCount().toString()
+        binding.button.setOnClickListener {
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
+    }
+}
---
+++ app/src/main/java/com/sample/viewmodeldemo/MainActivityViewModel.kt
@@ -0,0 +1,14 @@
+package com.sample.viewmodeldemo
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    private var count = 0
+
+    fun getCurrentCount(): Int{
+        return count
+    }
+    fun getUpdatedCount() : Int{
+        return ++count
+    }
+}
